a8fdc669c4b40bda17c42fa54084d8106fe7b44e
yousef kenawy <yosefkeno3100@gmail.com>
2024-12-04 12:06:41 +0200

    First Commit

---
+++ ArrayLab.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <climits>
+
+int main (){
+    //first question
+    // int num[5]={100,200,300,400,500};
+    // int sum=0;
+    // int avg=0;
+    // for(int i=0;i<5;i++){
+    //     sum+=num[i];
+    //     avg = sum/5;
+    // }
+    // std::cout<<sum<<std::endl;
+    // std::cout<<avg<<std::endl;
+
+
+    //second question
+    // int num[5]={100,200,300,400,500};
+    // int max = INT_MIN;
+    // int min = INT_MAX;
+    // for(int i=0;i<5;i++){
+    //     if(num[i]>max){
+    //         max=num[i];
+    //     }
+    //     if(num[i]<min){
+    //         min=num[i];
+    //     }
+    // }
+    // std::cout<<max<<std::endl;
+    // std::cout<<min<<std::endl;
+
+
+    //third question
+    // int num[5]={100,200,300,400,500};
+    // for (int i = 5 - 1; i >-1; i--) {
+    //     std::cout<<num[i]<<" ";
+    // }
+
+
+    //fourth question
+    // int num [5]={100,200,300,400,500};
+    // int element;
+    // std::cout<<"Enter the element you want to search: ";
+    // std::cin>>element;
+    // bool found = false;
+    // for(int i=0;i<5;i++){
+    //     if(num[i]==element){
+    //         found = true;
+    //         std::cout<<"The element is at index: "<<i<<std::endl;
+    //         break;
+    //     }
+    // }
+    // if(!found){
+    //     std::cout<<"Element not found"<<std::endl;
+    // }
+
+
+    //fifth question
+    // int num[5]={400,300,200,500,100};
+    // for(int i=0;i<5;i++){
+    //     for(int j=i+1;j<5;j++){
+    //         if(num[i]>num[j]){
+    //             int temp=num[i];
+    //             num[i]=num[j];
+    //             num[j]=temp;
+    //         }
+    //     }
+    // }
+    // for(int i=0;i<5;i++){
+    //     std::cout<<num[i]<<" ";
+    // }
+
+
+
+    //sixth question
+    // int num[10]={200,200,300,400,500,500,600,600,700,800};
+    // for(int i=0;i<8;i++){
+    //     for(int j=i+1;j<8;j++){
+    //         if(num[i]==num[j]){
+    //             std::cout<<num[i]<<" ";
+    //         }
+    //     }
+    // }
+
+
+    //seventh question
+    // int numbers[5] = {1, 2, 3, 4, 5};
+    // int position;
+    // std::cout << "Enter the value of position: ";
+    // std::cin >> position;
+    // int r = position % 5;
+    // for (int i = 0; i < 5; i++){
+    //     std::cout << numbers[(5 - r + i) % 5] << " ";
+    // }
+
+ 
+    //eighth question
+    // std::string name[5]={"mohamed","ahmed","ali","ziad","amir"};
+    // int maxName=0;
+    // int minName=INT_MAX;
+
+    // for(int i=0;i<5;i++){
+    //     if(name[i].length()>maxName){
+    //         maxName=name[i].length();
+    //     }
+    //     if(name[i].length()<minName){
+    //         minName=name[i].length();
+    //     }
+    // }
+
+    // for(int i=0;i<5;i++){
+    //     if(name[i].length()==maxName){
+    //         std::cout<<name[i]<<std::endl;
+    //     }
+    //     if(name[i].length()==minName){
+    //         std::cout<<name[i]<<std::endl;
+    //     }
+    // }
+
+    
+
+
+    
+    
+
+    
+    
+
+
+
+    
+}
